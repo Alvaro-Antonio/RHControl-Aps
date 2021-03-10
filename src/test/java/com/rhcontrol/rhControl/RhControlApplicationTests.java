0ed3b358b600965cb5e5034ebb79f9ce7794d0db
Alvaro Antonio <alvaro.antonio@dcx.ufpb.br>
2021-03-09 21:03:33 -0300

    fist commit

---
+++ src/test/java/com/rhcontrol/rhControl/RhControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhcontrol.rhControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
